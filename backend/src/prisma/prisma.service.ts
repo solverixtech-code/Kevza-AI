45f0876729702e4d8209d48d3ba5d74201d30306
Sahil Salve <sahillsalve79@gmail.com>
2026-09-08 18:20:48 +0530

    Fix Prisma Postgres adapter setup

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
@@ -6,6 +7,18 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is required');
+    }
+
+    super({
+      adapter: new PrismaPg({ connectionString }),
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
