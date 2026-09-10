53d109560331c20ec50d0e9e411485b7c019c619
team-aimbeat <team@aimbeat.com>
2026-09-10 10:59:46 +0530

    Add signup page

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(import.meta.dirname, 'index.html'),
         login: resolve(import.meta.dirname, 'login.html'),
+        signup: resolve(import.meta.dirname, 'signup.html'),
         forgotPassword: resolve(import.meta.dirname, 'forgot-password.html'),
         resetPassword: resolve(import.meta.dirname, 'reset-password.html'),
         verifyLogin: resolve(import.meta.dirname, 'verify-login.html'),
@@ -36,6 +37,7 @@ export default defineConfig({
     },
   },
 });
+
 
 
 
